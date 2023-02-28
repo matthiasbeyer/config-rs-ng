aa6400a008dbc0b10954f049d6e22f3ddffa15f2
Matthias Beyer <mail@beyermatthias.de>
2023-02-28 08:22:32 +0100

    Add test for nested custom config types
    With this patch we see that a user of the crate needs to implement
    FromConfigElement for their custom type as well if they want to nest
    types.
    
    I am not yet sure whether this is the way to go, but for the first step,
    this is sufficient. I think we want to add a derive macro for this as
    well, though.
    
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- tests/proc_macro.rs
+++ tests/proc_macro.rs
@@ -26,3 +26,56 @@ fn test() {
     let simple = SimpleConfig::construct_from(config.layers()).unwrap();
     assert_eq!(simple.s, "foo");
 }
+
+#[derive(Debug, config_rs_ng::ConfigConstructor)]
+struct OtherConfig {
+    optional: Option<i32>,
+
+    simple: SimpleConfig,
+}
+
+impl config_rs_ng::FromConfigElement for SimpleConfig {
+    type Error = config_rs_ng::FromConfigElementError;
+
+    fn from_config_element(element: &dyn config_rs_ng::ConfigElement) -> Result<Self, Self::Error> {
+        let map = element.as_map().ok_or_else(|| {
+            let found = element.get_type().name();
+            config_rs_ng::FromConfigElementError::TypeError {
+                expected: "map",
+                found,
+            }
+        })?;
+
+        Ok({
+            Self {
+                s: map
+                    .get("s")
+                    .ok_or_else(|| config_rs_ng::FromConfigElementError::NoElement {
+                        name: "s".to_string(),
+                        ty: "String".to_string(),
+                    })
+                    .and_then(String::from_config_element)?
+            }
+        })
+    }
+}
+
+const OTHER_CONFIG: &str = r#"
+    optional = 12
+    [simple]
+    s = "bar"
+"#;
+
+#[test]
+fn test_config() {
+    let config = Config::builder()
+        .load(Box::new(
+            StringSource::<TomlFormatParser>::new(OTHER_CONFIG.to_string()).unwrap(),
+        ))
+        .build()
+        .unwrap();
+
+    let other = OtherConfig::construct_from(config.layers()).unwrap();
+    assert_eq!(other.optional, Some(12));
+    assert_eq!(other.simple.s, "bar");
+}
