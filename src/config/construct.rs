11e415602e6f5da978b8a5e926fb2ba71d13aa44
Matthias Beyer <mail@beyermatthias.de>
2023-03-01 10:31:54 +0100

    Add FromConfigElementError::Custom
    The FromConfigElementError will protentially be constructed by users of
    the crate.
    Because we cannot anticipate how users might want to use that type, we
    should add a `Custom` variant that keeps a `Box<dyn std::error::Error>`,
    so that users can use their own error types with this type.
    
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/config/construct.rs
+++ src/config/construct.rs
@@ -24,6 +24,9 @@ where
 
 #[derive(Debug, thiserror::Error)]
 pub enum FromConfigElementError {
+    #[error(transparent)]
+    Custom(#[from] Box<dyn std::error::Error>),
+
     #[error("Type error. Expected '{expected}', got '{found}'")]
     TypeError {
         expected: &'static str,
